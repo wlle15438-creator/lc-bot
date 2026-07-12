cda7cafe2fa3e3366b4ca9bba95d24f31c9b6d3a
wlle15438-creator <wlle15438@gmail.com>
2026-07-12 20:14:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,20 @@
-import { createBotClient, type BotClient } from "./client";
-import { allCommands } from "./commandRegistry";
-import { deploySlashCommands } from "./deployCommands";
-import { registerReady } from "./events/ready";
-import { registerInteractionCreate } from "./events/interactionCreate";
-import { registerMessageCreate } from "./events/messageCreate";
-import { registerGuildMemberAdd } from "./events/guildMemberAdd";
-import { registerGuildMemberRemove } from "./events/guildMemberRemove";
-import { registerVoiceStateUpdate } from "./events/voiceStateUpdate";
-import { registerGuildCreate } from "./events/guildCreate";
-import { startPeriodicResetScheduler } from "./leveling/periodicResetScheduler";
-import { startGiveawayPoller } from "./giveaways/giveawayManager";
-import { discordEnv } from "./env";
-import { logger } from "../lib/logger";
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ 
+    intents: [
+        GatewayIntentBits.Guilds, 
+        GatewayIntentBits.GuildMessages, 
+        GatewayIntentBits.MessageContent
+    ] 
+});
 
-let client: BotClient | null = null;
+client.once('ready', () => {
+    console.log(`تم تشغيل البوت بنجاح ومستعد للعمل باسم: ${client.user.tag}`);
+});
 
-export function getBotClient(): BotClient | null {
-  return client;
-}
+client.on('messageCreate', (message) => {
+    if (message.content === 'بينج') {
+        message.reply('بونج! 🏓 البوت شغال وبأفضل حال.');
+    }
+});
 
-export async function startBot(): Promise<BotClient> {
-  const botClient = createBotClient();
-
-  for (const command of allCommands) {
-    botClient.commands.set(command.data.name, command);
-  }
-
-  registerReady(botClient);
-  registerInteractionCreate(botClient);
-  registerMessageCreate(botClient);
-  registerGuildMemberAdd(botClient);
-  registerGuildMemberRemove(botClient);
-  registerVoiceStateUpdate(botClient);
-  registerGuildCreate(botClient);
-  startPeriodicResetScheduler();
-  startGiveawayPoller(botClient);
-
-  try {
-    await deploySlashCommands();
-  } catch (error) {
-    logger.error({ err: error }, "Failed to register slash commands");
-  }
-
-  await botClient.login(discordEnv.token);
-  client = botClient;
-  return botClient;
-}
+client.login(process.env.DISCORD_TOKEN);
